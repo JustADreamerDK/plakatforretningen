8938151acc74c23a79c1da08f56e118651541d56
Katja Kjer <Kalle3008@hotmail.com>
2019-05-08 11:51:10 +0200

    card

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="UTF-8">
+    <title>Plakatforretningen.dk | Blog</title>
+    <link href="css/reset.css" rel="stylesheet">
+    <link href="css/css.css" rel="stylesheet">
+    <link rel="icon" type="image/png" href="img/tinyicon.png">
+</head>
+<body class="flex-column center">
+    <section class="content">
+        <div class="card m-tb-20">
+            <div class="b-brown p-10">
+                <h2 class="f-white bold">Har du set denne mand? Bliv inde og lås jeres dør!</h2>
+            </div>
+            <div class="flex between m-lr-10">
+                <h3 class="m-tb-10 bold">Andet</h3>
+                <h3 class="m-tb-10 bold">08-05-2019</h3>
+            </div>
+            <img class="image" src="img/5900.jpg">
+            <p class="m-tb-10 m-lr-20">Nulla pharetra, turpis vel laoreet porta, ligula tellus dictum magna, bibendum vehicula magna diam nec lorem. Curabitur at laoreet lectus, sit amet varius est. Nunc at risus varius leo rhoncus auctor vel quis augue. Nunc nulla est, scelerisque eget ex et, vulputate aliquam leo. Integer ut sapien nec tortor cursus luctus at at elit. Proin lectus tellus, euismod eget pellentesque in, sodales eget velit. Cras vel laoreet purus, ac lobortis velit. Nullam dolor odio, rutrum quis scelerisque id, auctor eget lectus. Mauris gravida eros tortor, a consectetur eros viverra a. Donec auctor bibendum purus at cursus. Pellentesque </p>
+            <a class="f-white flex-column right" href="">
+                <h3 class="knap">
+                    Læs mere
+                </h3>
+            </a>
+        </div>
+    </section>
+</body>
+</html>
