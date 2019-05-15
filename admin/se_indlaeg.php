7b9be379945e6e0c24c4968de2b6360b42067ed0
Katja Kjer <Kalle3008@hotmail.com>
2019-05-15 12:23:21 +0200

    Struktur på admin side

--- admin/se_indlaeg.php
+++ admin/se_indlaeg.php
@@ -17,14 +17,7 @@ include "../phpcode/crud.php";
     <section class="content flex">
         <?php include "menu.php"; ?>
         <div class="admin-content p-10 m-tb-20">
-            <div class="flex-column center">
-            <h2>Administration af Plakatforretningen.dks inspirationsblog</h2>
-        </div>
-        <p class="m-tb-20">
-            Herinde kan du se alle indlægs på bloggen. Du kan redigere i dem og du kan slette dem, hvis de ikke længere er aktuelle.
-            Du kan desuden se alle kommentarer til indlæggene, ligesom du kan slette i dem, hvis de ikke er passende.
-            Sidst, men ikke mindst har du mulighed for at se alle administratorerne, tilføje flere og slette de eksisterende. 
-        </p>
+            Se indlæg
         </div>
     </section>
 </body>
